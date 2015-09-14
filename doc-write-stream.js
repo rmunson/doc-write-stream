e1a40f8502528a0dd8d4340b8271580bd55e37ab
Rusell Munson <russ.munson@gmail.com>
2015-09-14 15:36:42 -0400

    Better support for scripts that live deeper in the subtree.  Using a doc frag as well, to minimize dom hits

--- doc-write-stream.js
+++ doc-write-stream.js
@@ -2,13 +2,13 @@ define(function(require,exports,module){
     var streamDocument = require('./lib/streamDocument');
 
     var STRING = "string";
+
     return function(target,html){
         var el = target,
             stream;
-        if(typeof target === 'string'){
+        if(typeof target === STRING){
             el = document.querySelector(target);
         }
-        // streams.push(stream);
         stream = streamDocument(el,html);
         stream.start();
         return stream;
